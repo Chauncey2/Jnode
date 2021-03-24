7fa1c08e1ed9bcf9fa7d84137604866db5df2459
gaochangju <gaochangju95@163.com>
2021-03-25 01:52:39 +0800

    继承

--- src/flamingo/Main.java
+++ src/flamingo/Main.java
@@ -1,7 +1,53 @@
 package flamingo;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.XPath;
+
+class Person {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Age must be > 0");
+        }
+    }
+}
+
+class Student extends Person {
+    private float score;
+
+    public float getScore() {
+        return this.score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+}
+
+
 public class Main {
-    public static void main(String[] args){
-        //
+    public static void main(String[] args) {
+        Student stu = new Student();
+        stu.setName("jone");
+        stu.setAge(18);
+        stu.setScore(90);
+
+        System.out.println("Hello world");
+
     }
 }
